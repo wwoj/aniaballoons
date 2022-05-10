604aeb2883aaccb4580a4e8c0b7c37ce83895921
wwoj <wojcik.wojtek@yahoo.pl>
2022-05-10 18:43:37 +0200

    Home page update

--- src/Views/home.js
+++ src/Views/home.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import MainPhoto from '../Pictures/1.JPG'
 
 const home = ()=>{
-    return(
-        <div>Home page</div>
-    )
+    return (
+      <section className='home-container'>
+        <div className='ds-flex-sb home-img-container align-center'>
+            <img className='w-100' src={MainPhoto} alt='Home picture'/>
+        </div>
+        <div>Container for the text from Anna</div>
+        <div>Container for the vew picture in small boxes</div>
+      </section>
+    );
 }
 export default home;
