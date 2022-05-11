e46f58e1d589a66be8111ce2e2ffcf2a643c7b4b
wojcikewelina <gargula.ewe@gmail.com>
2022-05-11 18:24:08 +0200

    Footer update

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -9,29 +9,28 @@ import "../Styles/footer.scss";
 
 const Footer = () => {
     return (
-
         <footer className="box two good">
             <ul className="inner-footer">
                 <li>
                     <ul>
                         <li className="column-head">Pomoc</li>
-                        <li>Pytania i odpowiedzi</li>
+                        <li>Polityka Cookies</li>
                         <li>Regulamin</li>
                     </ul>
                 </li>
                 <li>
                     <ul>
-                        <li className="column-head">O nas</li>
-                        <li>Kontakt</li>
-                        <li>Informacja o firmie</li>
+                        <li className="column-head">Kontakt</li>
+                        <li>aaaaa@gmail.com</li>
+                        <li>019-0000-000</li>
                     </ul>
                 </li>
                 <li>
                     <ul>
                         <li className="column-head">Social media</li>
-                        <li><span><FontAwesomeIcon icon={faInstagramSquare} size="2x" /></span>
-                            <span><FontAwesomeIcon icon={faFacebookSquare} size="2x" /></span>
-                            <span><FontAwesomeIcon icon={faYoutubeSquare} size="2x" /></span></li>
+                        <li><span><a href="https://www.instagram.com"><FontAwesomeIcon icon={faInstagramSquare} size="2x" /></a></span>
+                            <span><a href='https://www.facebook.com>'><FontAwesomeIcon icon={faFacebookSquare} size="2x" /></a></span>
+                            <span><a href="https://www.youtube.com"><FontAwesomeIcon icon={faYoutubeSquare} size="2x" /></a></span></li>
 
                     </ul>
                 </li>
